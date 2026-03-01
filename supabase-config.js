8b3978aad7f4e72fd073d942fea84847aa269b65
Garekrt <garekrt@gmail.com>
2026-03-01 23:40:22 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://nhsvadkqagsqgirvoibg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oc3ZhZGtxYWdzcWdpcnZvaWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzQ1MjQsImV4cCI6MjA4NzU1MDUyNH0.v0PPOfmX1p_sHkV2ZwzaH8gxr7VwN9MMRB1AclEOhvQ';
 
-// Usiamo 'sb' come nome variabile per non andare in conflitto con l'oggetto globale della libreria
+// Usiamo 'sb' come nome universale
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
