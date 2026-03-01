1c992820d85c7a948bbf682d9a5dc8200ed91b96
Garekrt <garekrt@gmail.com>
2026-03-01 17:09:20 +0100

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,5 @@
-// js/supabase-config.js
 const SUPABASE_URL = 'https://nhsvadkqagsqgirvoibg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5oc3ZhZGtxYWdzcWdpcnZvaWJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NzQ1MjQsImV4cCI6MjA4NzU1MDUyNH0.v0PPOfmX1p_sHkV2ZwzaH8gxr7VwN9MMRB1AclEOhvQ';
 
-// Usiamo supabaseClient per non andare in conflitto con l'oggetto globale 'supabase'
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Inizializziamo il client come 'sb'
+const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
